9a567f2b40cf1f0c4f5dd9c2b9d3c01a9cf980f7
ding <ding_1995_7@163.com>
2017-02-14 10:53:49 +0800

    Initial commit

---
+++ src/main/java/com/trc/favorites/Application.java
@@ -0,0 +1,23 @@
+package com.trc.favorites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+/**
+ * Application
+ * Created by hzdzf on 2017/2/13.
+ */
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
---
+++ src/main/java/com/trc/favorites/util/CommonUtil.java
@@ -0,0 +1,9 @@
+package com.trc.favorites.util;
+
+/**
+ * Created by hzdzf on 2017/2/14.
+ */
+public class CommonUtil {
+
+
+}
